c141c6c98e8065941a5a31d1f2a0bc40f3403ce3
Jerry Lee <lzjjerrywoodylee@gmai.com>
2018-01-03 22:52:01 +0900

    change logic

--- src/index.js
+++ src/index.js
@@ -5,28 +5,32 @@ const NfcpyId = require('node-nfcpy-id').default;
 const nfc = new NfcpyId().start();
 // const nfc = new NfcpyId({ scriptPath: __dirname, scriptFile: './lib/reader.py' }).start();
 const Fetch = require('./lib/fetch_url');
+const log4js = require('./lib/logger');
 const path = require('path');
 const YAML = require('yamljs');
 const config = YAML.load(path.join(__dirname, '../conf.yml'));
 const f = new Fetch(config.host + config.urls.punch);
 
-nfc.on('touchstart', (card) => {
-  console.log('touchstart', 'id:', card.id, 'type:', card.type);
+log4js.info('Service Started');
+
+nfc.on('touchstart', async (card) => {
+  log4js.info('touchstart', 'id:', card.id, 'type:', card.type);
   // { type: 3, event: 'touchstart', id: '01010310xxxxx' }
   // console.log(card);
   const { id, type } = card;
-  f.post({ id, type });
+  const res = await f.post({ card_uid: id, card_type: type });
+  log4js.info(res);
 });
 
 nfc.on('touchend', (card) => {
-  console.log('touchend');
+  log4js.info('touchend');
   // { event: 'touchend' }
   // console.log(card);
 });
 
 nfc.on('error', (err) => {
   // standard error output (color is red)
-  console.error('\u001b[31m', err, '\u001b[0m');
+  log4js.error(err);
 });
 
-const server = require('./page/server');
+// const server = require('./page/server');
